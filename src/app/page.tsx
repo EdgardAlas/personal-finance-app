3d7566223ef3e269d20b95f97ee9e63d389c2a13
Edgard Alas <edgardalasu@gmail.com>
2024-08-27 00:03:39 -0600

    feat: add css variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
 	return (
 		<>
 			<h1 className='text-center text-4xl font-bold'>Hello World</h1>
+			<Button>Click me</Button>
 		</>
 	);
 }
-
