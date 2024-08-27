ac4daab7c21cd5eb5b881855e40c9055e8b89f58
Edgard Alas <edgardalasu@gmail.com>
2024-08-27 15:23:47 -0600

    refactor: Add icon prop to Input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,33 @@
 import * as React from 'react';
 
 import { cn } from '@/lib/cn';
+import { LucideProps } from 'lucide-react';
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+	icon?: React.ComponentType<LucideProps>;
+};
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-	({ className, type, ...props }, ref) => {
+	({ className, type, icon: Icon, ...props }, ref) => {
 		return (
-			<input
-				type={type}
-				className={cn(
-					'bg-theme-white text-theme-gray-900 flex h-[2.8125rem] w-full rounded-md border border-input px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
-					className
+			<div className='relative'>
+				{Icon && (
+					<Icon className='absolute left-3 top-1/2 size-4 -translate-y-1/2 transform text-theme-gray-500' />
 				)}
-				ref={ref}
-				{...props}
-			/>
+
+				<input
+					type={type}
+					className={cn(
+						'flex h-[2.8125rem] w-full rounded-md border border-input bg-theme-white px-3 py-2 text-sm text-theme-gray-900 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+						{
+							'ps-9': Icon,
+						},
+						className
+					)}
+					ref={ref}
+					{...props}
+				/>
+			</div>
 		);
 	}
 );
