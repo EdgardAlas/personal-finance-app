abad73c1ba6fed897e7c12508d98048f0dc6f3c3
Edgard Alas <edgardalasu@gmail.com>
2024-09-10 10:51:19 -0600

    refactor: Update transactions-pagination component to use client-side rendering

--- src/app/(dashboard)/transactions/page.tsx
+++ src/app/(dashboard)/transactions/page.tsx
@@ -1,10 +1,13 @@
-'use client';
-
 import { SearchTransactions } from '@/app/(dashboard)/transactions/_containers/search-transactions';
 import { Transactions } from '@/app/(dashboard)/transactions/_containers/transactions';
 import { TransactionsPagination } from '@/app/(dashboard)/transactions/_containers/transactions-pagination';
 import { PageTitle } from '@/components/ui/page-title';
 import { VanillaCard } from '@/components/ui/vanilla-card';
+
+export const metadata = {
+	title: 'Transactions',
+	description: 'Here you can view all your transactions and filter them.',
+};
 
 const TransactionsPage = () => {
 	return (
