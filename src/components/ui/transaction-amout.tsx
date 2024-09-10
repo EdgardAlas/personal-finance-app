791d2b39f0999b85926ba96d6d4726a8a1018d31
Edgard Alas <edgardalasu@gmail.com>
2024-09-10 10:50:13 -0600

    refactor: Update TransactionAmout component to accept additional props

--- src/components/ui/transaction-amout.tsx
+++ src/components/ui/transaction-amout.tsx
@@ -1,17 +1,26 @@
 import { currencyFormat } from '@/helpers/currency-format';
 import { cn } from '@/lib/cn';
-import React from 'react';
+import React, { ComponentProps } from 'react';
 
-interface TransactionAmoutProps {
+interface TransactionAmoutProps extends ComponentProps<'p'> {
 	amount: number;
 }
 
-export const TransactionAmout = ({ amount }: TransactionAmoutProps) => {
+export const TransactionAmout = ({
+	amount,
+	className,
+	...rest
+}: TransactionAmoutProps) => {
 	return (
 		<p
-			className={cn('fz-preset-4-bold', {
-				'text-theme-green': amount > 0,
-			})}
+			className={cn(
+				'fz-preset-4-bold',
+				{
+					'text-theme-green': amount > 0,
+				},
+				className
+			)}
+			{...rest}
 		>
 			{amount > 0 ? '+' : '-'}
 			{currencyFormat(Math.abs(amount))}
